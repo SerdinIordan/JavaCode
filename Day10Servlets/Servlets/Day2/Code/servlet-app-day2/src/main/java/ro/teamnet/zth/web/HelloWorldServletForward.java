527000d23f482d9148674202757ab2249d6ca524
Iordan.Serdin <iordan.serdin@gmail.com>
2017-07-19 07:51:04 -0700

    Mesaj Servlets2

---
+++ Day10Servlets/Servlets/Day2/Code/servlet-app-day2/src/main/java/ro/teamnet/zth/web/HelloWorldServletForward.java
@@ -0,0 +1,25 @@
+package ro.teamnet.zth.web;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by Iordan.Serdin on 7/18/2017.
+ */
+public class HelloWorldServletForward extends HttpServlet {
+
+        public void getDate(HttpServletRequest request,HttpServletResponse response) throws ServletException, IOException {
+
+
+                response.getWriter().write("Hello <b>" + request.getParameter("user")+"</b> from the Forward Servlet! "
+                +request.getAttribute("testAttribute"));
+
+
+        }
+
+
+
+}
